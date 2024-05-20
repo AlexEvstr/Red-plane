259a2439664cd01a7998e699446acf836dc307f6
Alexander Evstratov <alexdrugev@me.com>
2024-05-20 13:24:28 +0300

    Create project, add endless BG.

---
+++ Assets/_Scripts/BackgroundMovement.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class BackgroundMovement : MonoBehaviour
+{
+    private float _speed = 2.0f;
+    private Vector2 _zeroPosition;
+    private float _middleOfBackground;
+
+    private void Start()
+    {
+        //DontDestroyOnLoad(gameObject);
+        _zeroPosition = transform.position;
+        _middleOfBackground = GetComponent<BoxCollider2D>().size.x / 2;
+    }
+
+    private void FixedUpdate()
+    {
+        transform.Translate(Vector2.left * Time.deltaTime * _speed);
+        if (transform.position.x < (_zeroPosition.x - _middleOfBackground))
+            transform.position = _zeroPosition;
+    }
+}
